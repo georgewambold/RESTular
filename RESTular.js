e65b6a90c104c58eaf432774478a6c7e47c9cf85
gaw1990 <georgewambold@gmail.com>
2015-11-29 23:00:10 -0600

    much better html/css, jquery for mouse over, click to display

---
+++ RESTular.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$(".submission-box").on("keyup", function() {
+		$(".golden-text").text($(this).val().toLowerCase())
+	});
+});
+
+$(document).ready(function(){
+   $('tr.route').mouseover(function(){
+      $(this).addClass('hover');
+   });
+        
+   $('tr.route').mouseout(function(){
+      $(this).removeClass('hover');
+   });
+});
+
+$(document).ready(function(){
+	$(".route").on("click", function() {
+		$(this).next().toggle()
+	});
+});
