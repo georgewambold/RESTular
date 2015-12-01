64cacf30881e9d850805a3aed5f6dc37661c5aaa
gaw1990 <georgewambold@gmail.com>
2015-11-30 21:51:31 -0600

    about to try something sketchy, need to commit

--- RESTular.js
+++ RESTular.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 	$(".submission-box").on("keyup", function() {
     $(".golden-text").text($(this).val().toLowerCase())		
     $(".g-text-singular").text(capitalizeFirstLetter(pluralize($(this).val(), 1)))
+    $(".g-text-s-dc").text((pluralize($(this).val(), 1)))
 	});
 });
 
@@ -25,6 +26,10 @@ $(document).ready(function(){
 		$(this).next().toggle()
 	});
 });
+
+
+
+
 
 
 /* global define */
