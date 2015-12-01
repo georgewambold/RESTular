8958ba7987e93a18a0a695b7fa4510b6882c24c6
gaw1990 <georgewambold@gmail.com>
2015-11-30 20:10:31 -0600

    add more complex pluralization

--- resources/uncommonPlurals.js
+++ resources/uncommonPlurals.js
@@ -1,50 +1,3 @@
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-function singularize(string) {
-  var stringEnd = string.length - 1
-
-  if (plurals[string]) {
-    return plurals[string]
-  }  else if (string.substr((stringEnd - 2), stringEnd) === "ies") {
-    return (string.substr(0, stringEnd - 2) + "y")
-  } else if (string.substr((stringEnd - 2), stringEnd) === "ses") {
-    return string.substr(0, stringEnd - 1) 
-  } else if (string.substr(stringEnd) === "s") {
-    return string.substr(0, (stringEnd))
-  } else {
-    return string
-  }
-}
-
-$(document).ready(function() {
-	$(".submission-box").on("keyup", function() {
-    $(".golden-text").text($(this).val().toLowerCase())
-		$(".g-text-singular").text(capitalizeFirstLetter(singularize($(this).val())))
-	});
-});
-
-$(document).ready(function(){
-   $('tr.route').mouseover(function(){
-      $(this).addClass('hover');
-   });
-        
-   $('tr.route').mouseout(function(){
-      $(this).removeClass('hover');
-   });
-});
-
-$(document).ready(function(){
-	$(".route").on("click", function() {
-		$(this).next().toggle()
-	});
-});
-
-
-
-
-
 
 
 
@@ -175,5 +128,3 @@ var plurals = {
   'wolves': 'wolf',
   'women': 'woman' 
 }
-
-
