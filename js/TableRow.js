14de23b80dd9388645ced97639ca932998942d64
gaw1990 <georgewambold@gmail.com>
2016-02-09 11:12:58 -0600

    Change JS class name TableRows to TableRow to more accurately describe it's job

--- js/TableRow.js
+++ js/TableRow.js
@@ -1,4 +1,4 @@
-TableRows = function(object) {
+TableRow = function(object) {
   this.type = object["type"];
   this.path = object["path"];
   this.viewFile = object["viewFile"];
@@ -7,7 +7,7 @@ TableRows = function(object) {
   this.suggested = object["suggested"];
 }
 
-TableRows.prototype.createRows = function() {
+TableRow.prototype.createRows = function() {
 
   var html = "<tr class='route'><td>" + this.type + "</td><td>" + this.path + "</td><td>" + this.viewFile + "</td><td>" + this.description + "</td></tr><tr class='route-info'><td colspan='2'><span class='temp-header'>Path Template:</span><pre class='route-pre'>" + this.vanilla + "</pre><div class='copy-prompt'>^click to copy^</div></td><td colspan='2'><span class='temp-header'>Suggestions:</span><span class='header-caption'> (to be taken with a grain of salt)</span><pre class='route-pre'>" + this.suggested + "</pre><div class='copy-prompt'>^click to copy^</div></td></tr>"
 
