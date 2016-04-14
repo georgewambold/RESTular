313266979f639e9391a4c0c79083c1ba80f3174e
gaw1990 <georgewambold@gmail.com>
2016-04-14 12:13:05 -0500

    Update folder hierarchy for angular conventions

---
+++ js/controllers/TableCtrl.js
@@ -0,0 +1,26 @@
+restularApp.controller('TableCtrl', function($scope, $filter, $compile){
+
+  $scope.routes = singularRoutes
+  $scope.resource = "articles"
+  $scope.nestedResource = ""
+
+  $scope.$watchGroup(['resource', 'nestedResource'], function (newValues){
+    downcaseNewValue = $filter('lowercase')(newValues[0])
+
+    // Singular resource variables
+    $scope.singularDowncaseResource = $filter('transform')(downcaseNewValue, ['singularize'])
+    $scope.singularPropercaseResource = $filter('transform')(downcaseNewValue, ['singularize'])
+    $scope.pluralDowncaseResource = downcaseNewValue
+
+    // Nested resource 
+    if (newValues[1]) {
+      $scope.routes = nestedRoutes
+      downcaseNestedResource = $filter('lowercase')(newValues[1])
+
+      $scope.singularDowncaseNestedResource = $filter('transform')(downcaseNestedResource, ['singularize'])
+      $scope.pluralDowncaseNestedResource = downcaseNestedResource
+    } else {
+      $scope.routes = singularRoutes
+    }
+  });
+});
