ce99763e687fef6b9547ad75459501040eea810d
gaw1990 <georgewambold@gmail.com>
2016-04-14 12:31:33 -0500

    Checked local angular version against html/JQ live versiona and all dynamic elements match

--- js/controllers/TableCtrl.js
+++ js/controllers/TableCtrl.js
@@ -5,16 +5,19 @@ restularApp.controller('TableCtrl', function($scope, $filter, $compile){
   $scope.nestedResource = ""
 
   $scope.$watchGroup(['resource', 'nestedResource'], function (newValues){
+
     downcaseNewValue = $filter('lowercase')(newValues[0])
+    upcaseNewValue = $filter('capitalize')(newValues[0])
 
     // Singular resource variables
     $scope.singularDowncaseResource = $filter('transform')(downcaseNewValue, ['singularize'])
-    $scope.singularPropercaseResource = $filter('transform')(downcaseNewValue, ['singularize'])
+    $scope.singularPropercaseResource = $filter('transform')(upcaseNewValue, ['singularize'])
     $scope.pluralDowncaseResource = downcaseNewValue
 
     // Nested resource 
     if (newValues[1]) {
       $scope.routes = nestedRoutes
+
       downcaseNestedResource = $filter('lowercase')(newValues[1])
 
       $scope.singularDowncaseNestedResource = $filter('transform')(downcaseNestedResource, ['singularize'])
