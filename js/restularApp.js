68f6034b20af2ff71ade8de9d13d810a0a77a3cc
gaw1990 <georgewambold@gmail.com>
2016-10-10 17:00:00 -0500

    Trying to get zcb to work

--- js/restularApp.js
+++ js/restularApp.js
@@ -1,1 +1,9 @@
-var restularIndex = angular.module('restularIndex', ['ngInflection', 'ngSanitize']);
+var restularIndex = angular.module('restularIndex', ['ngInflection', 'ngSanitize', 'zeroclipboard']).
+    config(['uiZeroclipConfigProvider', function(uiZeroclipConfigProvider) {
+
+    // config ZeroClipboard
+    uiZeroclipConfigProvider.setZcConf({
+      swfPath: 'libraries/ZeroClipboard.swf'
+    });
+
+  }])
